5505bf20f0203f8d457f39cf652aa2594a794efa
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2019-07-21 17:21:59 +0300

    url2GSI: Update script

--- url2GSI.sh
+++ url2GSI.sh
@@ -5,10 +5,11 @@ srctype=$2
 
 mkdir downloads
 
-wget -U "Mozilla/5.0" https://dl.google.com/developers/android/qt/images/gsi/gsi_gms_arm64-exp-QPP5.190530.016-5733663.zip -O downloads/temp.zip
+wget -U "Mozilla/5.0" $url -O downloads/temp.zip
 ./zip2img.sh downloads/temp.zip
 mkdir system
 mount cache/system.img system
 ./make.sh system $srctype AB
+./make.sh system $srctype Aonly
 
 ls -ln out/
